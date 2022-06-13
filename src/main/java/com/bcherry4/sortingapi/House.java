5ec8f437eaec42488df371cd35e11fc0a4a94f6e
bcherry4 <bcherry4@ford.com>
2022-06-13 15:35:19 -0600

    Remove part of model

--- src/main/java/com/bcherry4/sortingapi/House.java
+++ src/main/java/com/bcherry4/sortingapi/House.java
@@ -26,28 +26,5 @@ public class House {
   @Column(name = "image_url")
   private String imageUrl;
 
-public Long getId() {
-    return null;
-}
 
-public Object getName() {
-    return null;
-}
-
-public void setName(Object name2) {
-}
-
-public Object getDetails() {
-    return null;
-}
-
-public Object getImageUrl() {
-    return null;
-}
-
-public void setDetails(Object details2) {
-}
-
-public void setImageUrl(Object imageUrl2) {
-}
 }
