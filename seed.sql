20988ccf5299ec7e91bd5fa40decbeb0a4969402
bcherry4 <bcherry4@ford.com>
2022-06-14 12:25:45 -0600

    Commit for kyle to look

---
+++ seed.sql
@@ -0,0 +1,11 @@
+TRUNCATE house;
+
+INSERT INTO house(id, name, details, image_url) VALUES(
+    1, 'Gryffindor', 'Bravery, helping others, and chivalry.', 'https://i.pinimg.com/originals/44/d4/5f/44d45f5786e8371255e8332e8bc456c5.jpg'
+), (
+    2, 'Slytherin', 'Ambition, cunningness, heritage, and resourcefulness.', 'https://www.logolynx.com/images/logolynx/53/5390e974544de6279c4d9cb6253e3a2c.jpeg'
+), (
+    3, 'Ravenclaw', 'Intelligence, knowledge, planning ahead, and wit.', 'https://i.pinimg.com/736x/7f/d2/73/7fd273912c5104ca81124464b8f73937.jpg'
+), (
+    4, 'Hufflepuff', 'Hard work, patience, loyalty, and fair play.', 'https://i.pinimg.com/originals/c7/ce/d5/c7ced5c1092dff4b2aa3ac89b6974ac2.jpg'
+);
